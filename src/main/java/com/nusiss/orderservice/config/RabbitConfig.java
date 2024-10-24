8595be9de593307569cdfb53f5662ecc3cc89fed
zenmang <305611332@qq.com>
2024-10-24 10:31:22 +0800

    #zengmeng_remove unsed rabbit config and move application to config repo

--- src/main/java/com/nusiss/orderservice/config/RabbitConfig.java
+++ src/main/java/com/nusiss/orderservice/config/RabbitConfig.java
@@ -20,7 +20,6 @@ public class RabbitConfig {
     public static final String CONFIRM_QUEUE = "confirm.queue";
     public static final String ROLLBACK_QUEUE = "rollback.queue";
     public static final String EXCHANGE = "order.exchange";
-    public static final String ORDER_CREATED_QUEUE = "order.created.queue";
 
     @Bean
     public Queue orderQueue() {
@@ -40,11 +39,6 @@ public class RabbitConfig {
     @Bean
     public Queue rollbackQueue() {
         return new Queue(ROLLBACK_QUEUE, true);
-    }
-
-    @Bean
-    public Queue orderCreatedQueue() { // 新增队列的 bean
-        return new Queue(ORDER_CREATED_QUEUE, true);
     }
 
     @Bean
@@ -73,11 +67,6 @@ public class RabbitConfig {
     }
 
     @Bean
-    public Binding orderCreatedBinding(Queue orderCreatedQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(orderCreatedQueue).to(exchange).with("order.created"); // 绑定新队列
-    }
-
-    @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
@@ -89,57 +78,3 @@ public class RabbitConfig {
         return new Jackson2JsonMessageConverter();
     }
 }
-
-// public class RabbitConfig {
-//
-//     public static final String ORDER_QUEUE = "order.queue";
-//     public static final String INVENTORY_QUEUE = "inventory.queue";
-//     public static final String CONFIRM_QUEUE = "confirm.queue";
-//     public static final String ROLLBACK_QUEUE = "rollback.queue";
-//     public static final String EXCHANGE = "order.exchange";
-//
-//     @Bean
-//     public Queue orderQueue() {
-//         return new Queue(ORDER_QUEUE, true);
-//     }
-//
-//     @Bean
-//     public Queue inventoryQueue() {
-//         return new Queue(INVENTORY_QUEUE, true);
-//     }
-//
-//     @Bean
-//     public Queue confirmQueue() {
-//         return new Queue(CONFIRM_QUEUE, true);
-//     }
-//
-//     @Bean
-//     public Queue rollbackQueue() {
-//         return new Queue(ROLLBACK_QUEUE, true);
-//     }
-//
-//     @Bean
-//     public TopicExchange exchange() {
-//         return new TopicExchange(EXCHANGE);
-//     }
-//
-//     @Bean
-//     public Binding orderBinding(Queue orderQueue, TopicExchange exchange) {
-//         return BindingBuilder.bind(orderQueue).to(exchange).with("order.*");
-//     }
-//
-//     @Bean
-//     public Binding inventoryBinding(Queue inventoryQueue, TopicExchange exchange) {
-//         return BindingBuilder.bind(inventoryQueue).to(exchange).with("inventory.*");
-//     }
-//
-//     @Bean
-//     public Binding confirmBinding(Queue confirmQueue, TopicExchange exchange) {
-//         return BindingBuilder.bind(confirmQueue).to(exchange).with("confirm.*");
-//     }
-//
-//     @Bean
-//     public Binding rollbackBinding(Queue rollbackQueue, TopicExchange exchange) {
-//         return BindingBuilder.bind(rollbackQueue).to(exchange).with("rollback.*");
-//     }
-// }
