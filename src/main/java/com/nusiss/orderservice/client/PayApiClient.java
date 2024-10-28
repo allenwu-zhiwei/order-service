b110ae8bc7bc3f8ef2443a0e70dbf683a6998d92
jyc <jyc0421@foxmail.com>
2024-10-28 23:45:25 +0800

    jyc commit

--- src/main/java/com/nusiss/orderservice/client/PayApiClient.java
+++ src/main/java/com/nusiss/orderservice/client/PayApiClient.java
@@ -2,6 +2,7 @@ package com.nusiss.orderservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 
@@ -9,5 +10,5 @@ import java.math.BigDecimal;
 public interface PayApiClient {
     // 发起支付(待定，是否要提交订单的同时发起支付，如果删掉选择付款方式的界面就用这个发出请求)
     @GetMapping("/api/payment/pay")
-    public String submitPayment(String orderId);
+    public String submitPayment(@RequestParam("orderId")String orderId);
 }
