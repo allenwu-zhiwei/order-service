95b78d197ac51c8f7cc4138f99553160bf201c80
jyc <jyc0421@foxmail.com>
2024-10-28 23:49:20 +0800

    jyc commit

--- src/main/java/com/nusiss/orderservice/client/PayApiClient.java
+++ src/main/java/com/nusiss/orderservice/client/PayApiClient.java
@@ -10,5 +10,5 @@ import java.math.BigDecimal;
 public interface PayApiClient {
     // 发起支付(待定，是否要提交订单的同时发起支付，如果删掉选择付款方式的界面就用这个发出请求)
     @GetMapping("/api/payment/pay")
-    public String submitPayment(@RequestParam("orderId")String orderId);
+    public void submitPayment(@RequestParam("orderId")String orderId);
 }
